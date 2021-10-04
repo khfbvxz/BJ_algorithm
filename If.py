d35580bb9c9d1554392ebfa16156d8014bbf6f6a
YuHwan Kim <khfbvxz@naver.com>
2021-10-04 12:08:16 +0900

    Initial commit

---
+++ If.py
@@ -0,0 +1,25 @@
+# #두수 비교 
+# a , b = input().split()
+# a = int(a)
+# b = int(b)
+# if (a>b):
+#     print(">")
+# elif(a<b):
+#     print("<")
+# else:
+#     print("==")
+
+## 시험성적
+
+# a = int(input())
+# if(a>=90 and a<=100):
+#     print("A")
+# elif(a>=80):
+#     print("B")
+# elif(a>=70):
+#     print("C")
+# elif(a>=60):
+#     print("D")
+# else:
+#     print("F")
+
---
+++ oprater.py
@@ -0,0 +1,52 @@
+# print("Hello Wolrd!")
+
+# print("강한친구 대한육군")
+# print("강한친구 대한육군")
+
+'''
+3.
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+'''
+# str1 = "\    /\\"
+# print("\    /\\")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \(__)|")
+
+'''
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+'''
+# print("|\_/|")
+# print("|q p|   /}")
+# print('( 0 )"""\\')
+# print('|"^"`    |')
+# print("||_/=\\\\__|")
+
+# a,b,c= map(int,input().split())
+
+# print((a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c, sep='\n')
+
+# a,b,c= map(int, input().split())
+# print((a+b)%c)
+# print((a%c + b%c)%c)
+# print((a*b)%c)
+# print((a%c * b%c)%c)
+
+## 곱셈
+# a = input()
+# b = input()
+# a =int(a)
+# c = list(b)
+# print(a*int(c[2]))
+# print(a*int(c[1]))
+# print(a*int(c[0]))
+# print(a*int(b))
+
+
